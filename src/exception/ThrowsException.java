866cc08a6e72454e8f4b4445d0a05849d1770a41
rla951214 <rla951214@naver.com>
2021-04-15 23:51:24 +0900

    'init'

---
+++ src/exception/ThrowsException.java
@@ -0,0 +1,28 @@
+package exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ThrowsException {
+	
+	public Class loadClass(String fileName, String className) throws FileNotFoundException, ClassNotFoundException {
+		FileInputStream fis = new FileInputStream(fileName);
+		Class c = Class.forName(className);
+		return c;
+	}
+
+	public static void main(String[] args) {
+		
+		ThrowsException ex = new ThrowsException();
+		try {
+			ex.loadClass("b.txt", "java.lang.string");
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		} catch (ClassNotFoundException e) {
+			System.out.println(e);
+		}
+		System.out.println("end");
+
+	}
+
+}
